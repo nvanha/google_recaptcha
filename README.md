76ff6da7b1e3ca68653a90505e0cb2ff8f26cc13
Nguyễn Văn Hà <nguyenvanha111120@gmail.com>
2021-04-04 15:59:46 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# google_recaptcha
+### [Google reCAPTCHA in PHP Form][link] - [nvan.ha][website] 👋
+
+## Introduce
+- Improving the security of your website/form is extremely important.
+- I have recorded this PHP tutorials where you can learn how to protect your form by using Google reCaptcha v2.
+
+## Installation
+Visit [Google reCAPTCHA][link2], and create a label reCAPTCHA v2:
+
+---
+Go to settings to copy `Site key` and `Secret key` of `reCAPTCHA key`, then paste in lines 24 and 46 in file `index.php`
+
+## Path
+Index:
+```path
+localhost/google_recaptcha/index.php
+```
+
+### Connect with me:
+
+[<img align="left" alt="nvanha.com" width="22px" src="https://raw.githubusercontent.com/iconic/open-iconic/master/svg/globe.svg" />][website]
+[<img align="left" alt="nvanha | Facebook" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/facebook.svg" />][facebook]
+[<img align="left" alt="nvanha | Instagram" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/instagram.svg" />][instagram]
+
+[website]: https://nvanha.github.io/myweb
+[instagram]: https://www.instagram.com/_haa_nguyen
+[facebook]: https://www.facebook.com/nvh1120
+[link]: https://github.com/nvanha/google_recaptcha
---
+++ demo.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Google Captcha</title>
+    <link 
+		href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" 
+		rel="stylesheet"
+	/>
+    <link rel="stylesheet" href="style/reset.css" />
+    <link rel="stylesheet" href="style/style.css" />
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
+<body>
+<?php
+    if (isset($_GET['action']) && $_GET['action'] == 'send') {
+        $secret = 'your_secret_key';
+        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $_POST['g-recaptcha-response']);
+        $responseData = json_decode($verifyResponse);
+        if (!$responseData->success) {
+            $error = "You have not verified Captcha";
+        }
+?>
+<div class="container">
+    <div class="error">
+        <?php 
+            if(isset($error)) {
+                echo "<p>" . $error . "</p>"; 
+            } else {
+                echo "<p>Verified successfully</p>";  
+            }
+        ?>
+        <a href="demo.php">Back</a>
+    </div>
+</div>
+<?php } else { ?>
+<div class="container">
+    <form action="?action=send" class="form" method="post" enctype="multipart/form-data">
+        <p>Who are you?</p>
+        <div class="g-recaptcha" data-sitekey="your_site_key"></div>
+        <input type="submit" value="Submit" />
+    </form>
+</div>
+<?php } ?>
+</body>
+</html>
